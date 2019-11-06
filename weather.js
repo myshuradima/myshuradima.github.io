edcc707aab02ae5367ab5f36406015de11346219
myshuradima <44754035+myshuradima@users.noreply.github.com>
2019-11-06 16:12:25 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,43 +1,20 @@
-class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
+const PLACES = [
+  { name: "Москваᅠ", cityName: "Moscow, RU" }, 
+  { name: "Санкт-Петербургᅠ", cityName: "Saint Petersburg, RU" },
+  { name: "Киевᅠ", cityName: "Kyiv, UA" },
+  { name: "Одессаᅠ", cityName: "Odessa, UA" },
+  { name: "Чикагоᅠ", cityName: "Chicago, US" },
+  { name: "Лос Анджелесᅠ", cityName: "Los Angeles, US" },
+  { name: "Mаямиᅠ", cityName: "Miami, US" },
+  { name: "Санта Моникаᅠ", cityName: "Santa Monica, PH" },
+  { name: "Сан Хосеᅠ", cityName: "San Jose, US" },
+  { name: "Даунтаунᅠ", cityName: "Downton, GB" },
+  { name: "Бучаᅠ", cityName: "Bucha, UA" },
+  { name: "Нью-Йорк", cityName: "New York, US" }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+];
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
 
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-ReactDOM.render(
-  <FlavorForm />,
-  document.getElementById('root')
-);
 class WeatherDisplay extends Component {
   constructor() {
     super();
@@ -46,7 +23,7 @@ class WeatherDisplay extends Component {
     };
   }
   componentDidMount() {
-    const cityName = "Kyiv";
+    const cityName = this.props.cityName;
     const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
       cityName +
       "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
@@ -75,7 +52,50 @@ class WeatherDisplay extends Component {
     );
   }
 }
-ReactDOM.render(
-  <WeatherDisplay />,
-  document.getElementById('root')
-);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+           
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Выберите город</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} cityName={PLACES[activePlace].cityName} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
